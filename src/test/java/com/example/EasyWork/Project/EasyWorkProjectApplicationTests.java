857ac7dd8f7cec6134972c39c34ce8c25883b592
ChoiSungWon <spancho0810@naver.com>
2026-02-02 20:28:52 +0900

    init: 초기 프로젝트 설정

---
+++ src/test/java/com/example/EasyWork/Project/EasyWorkProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EasyWork.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyWorkProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
