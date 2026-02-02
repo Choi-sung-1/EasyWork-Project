156e9bd695e1486dd71e340b1f7337c0b39f7fdd
ChoiSungWon <spancho0810@naver.com>
2026-02-02 21:42:47 +0900

    feature: 데이터베이스 셋업

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE TBL_USER (
+                        USER_ID BIGINT PRIMARY KEY AUTO_INCREMENT,
+                        USER_LOGIN_ID VARCHAR(50),
+                        USER_PASSWORD VARCHAR(200),
+                        USER_NAME VARCHAR(50)
+);
